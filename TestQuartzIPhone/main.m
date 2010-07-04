116c09e684d38ab324864d3687d33adf498fa243
cd <cd@cpd.id.au>
2010-07-04 17:53:46 +1000

    test quartz iphone

---
+++ TestQuartzIPhone/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestQuartzIPhone
+//
+//  Created by Chris Davey on 5/07/09.
+//  Copyright none 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
