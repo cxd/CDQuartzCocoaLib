e4d80ec9a9f423d7e1366df9c297e408c10c47be
cd <cd@cpd.id.au>
2010-09-05 18:46:32 +1000

    added separate header for swapping between versions of sdk (ios or osx)

---
+++ CDQuartzGraph/Library_Header/CDQuartzGraphHeader.h
@@ -0,0 +1,11 @@
+/*
+ *  CDQuartzGraphHeader.h
+ *  CDQuartzGraph
+ *
+ *  Created by Chris Davey on 5/09/10.
+ *  Copyright 2010 none. All rights reserved.
+ *
+ */
+#import <Cocoa/Cocoa.h>
+#import "QuartzCocoaLib/QuartzCocoaLib.h"
+#import "CDGraph/CDGraph.h"
