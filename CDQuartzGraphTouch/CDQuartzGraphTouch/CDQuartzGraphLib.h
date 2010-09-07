59c5e551ee94585f54ee06eff80c9265a46fe95f
cd <cd@cpd.id.au>
2010-09-07 22:32:56 +1000

    added touch library project

---
+++ CDQuartzGraphTouch/CDQuartzGraphTouch/CDQuartzGraphLib.h
@@ -0,0 +1,29 @@
+/*
+ *  CDQuartzGraph.h
+ *  CDQuartzGraph
+ *
+ *  Created by Chris Davey on 14/08/10.
+ *  Copyright 2010 none. All rights reserved.
+ *
+ */
+
+#import "QuartzCocoaLib.h"
+#import "CDGraph.h"
+
+#import "ShapeDelegate.h"
+#import "Drawable.h"
+
+#import "AbstractGraphShape.h"
+#import "AbstractNodeShape.h"
+#import "AbstractConnectorShape.h"
+#import "BezierLineConnector.h"
+#import "CurvedRectangleNode.h"
+#import "CurvedRectanglePort.h"
+
+#import "CDQuartzNode.h"
+#import "CDQuartzEdge.h"
+#import "CDQuartzGraph.h"
+
+#import "UIGraphView.h"
+
+#import "ForceDirectedLayout.h"
