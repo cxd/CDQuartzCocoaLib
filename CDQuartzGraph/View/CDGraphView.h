c2737b76b8a26471eb07425b2afb81469f8f46b8
cd <cd@cpd.id.au>
2010-09-07 22:31:49 +1000

    moved view to subdirectory

---
+++ CDQuartzGraph/View/CDGraphView.h
@@ -0,0 +1,50 @@
+//
+//  CDGraphView.h
+//  CDQuartzGraph
+//
+//  Created by Chris Davey on 22/08/10.
+//  Copyright 2010 none. All rights reserved.
+//
+
+#import "CDQuartzGraphHeader.h"
+#import "CDQuartzGraph.h"
+#import "CDQuartzNode.h"
+#import "CDQuartzEdge.h"
+
+
+#import "ShapeDelegate.h"
+#import "Drawable.h"
+
+#import "AbstractGraphShape.h"
+#import "AbstractNodeShape.h"
+#import "AbstractConnectorShape.h"
+
+#import "GraphLayoutAlgorithm.h"
+
+/**
+ The Graph View provides support for drawing graphs within an NSView. 
+ **/
+@interface CDGraphView : NSView {
+	/**
+	 Internal graph instance.
+	 **/
+	CDQuartzGraph *graph;
+	
+	CDQuartzNode *trackNode;
+	
+	id<GraphLayoutAlgorithm> algorithm;
+}
+
+/**
+ Internal graph instance.
+ **/
+@property(retain) CDQuartzGraph *graph;
+
+@property(retain) CDQuartzNode *trackNode;
+
+/**
+ A reference to a graph layout algorithm.
+ **/
+@property(retain) id<GraphLayoutAlgorithm> algorithm;
+
+@end
