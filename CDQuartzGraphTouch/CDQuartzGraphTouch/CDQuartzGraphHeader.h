ee533caa9abc63ec46b94a11459c7dd6c429b8fa
cd <cd@cpd.id.au>
2010-09-07 22:32:47 +1000

    added touch library project

---
+++ CDQuartzGraphTouch/CDQuartzGraphTouch/CDQuartzGraphHeader.h
@@ -0,0 +1,11 @@
+/*
+ *  CDQuartzGraphHeader.h
+ *  CDQuartzGraph
+ *
+ *  Created by Chris Davey on 5/09/10.
+ *  Copyright 2010 none. All rights reserved.
+ *
+ */
+#import <UIKit/UIKit.h>
+#import "QuartzCocoaLib.h"
+#import "CDGraph.h"
