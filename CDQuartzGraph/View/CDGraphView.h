1757301ca57e1707f5fe465cc6e18167e7c10ca1
Chris Davey <cd@cpd.id.au>
2010-11-15 19:01:18 +1000

    using BSD license

--- CDQuartzGraph/View/CDGraphView.h
+++ CDQuartzGraph/View/CDGraphView.h
@@ -3,8 +3,17 @@
 //  CDQuartzGraph
 //
 //  Created by Chris Davey on 22/08/10.
-//  Copyright 2010 none. All rights reserved.
-//
+/**
+ Copyright (c) 2010, Chris Davey
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+ Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
 
 #import "CDQuartzGraphHeader.h"
 #import "CDQuartzGraph.h"
@@ -28,7 +37,14 @@
 /**
  The Graph View provides support for drawing graphs within an NSView. 
  **/
-@interface CDGraphView : NSView<IEditNodeDelegate> {
+@interface
+#ifdef UIKIT_EXTERN
+ CDGraphView : UIView<IEditNodeDelegate>
+#else
+ CDGraphView : NSView<IEditNodeDelegate> 
+#endif
+{
+	
 	/**
 	 Internal graph instance.
 	 **/
@@ -59,6 +75,20 @@
 	 **/
 	QModifierQueue *queue;
 	
+#ifdef UIKIT_EXTERN
+	/**
+	 Scroll view reference.
+	 Used if the view is nested within a parent view.
+	 **/
+	UIScrollView *parentScrollView;
+	
+	/**
+	 An editable field to allow the user to modify the
+	 text of a node.
+	 **/
+	UITextField *labelField;
+	
+#else
 	/**
 	 Scroll view reference.
 	 Used if the view is nested within a parent view.
@@ -71,6 +101,9 @@
 	 **/
 	NSTextField *labelField;
 	
+	
+#endif
+		
 }
 
 /**
@@ -111,6 +144,21 @@
  **/
 @property(assign) BOOL editLabel;
 
+#ifdef UIKIT_EXTERN
+
+/**
+ Scroll view reference.
+ Used if the view is nested within a parent view.
+ **/
+@property(retain) IBOutlet UIScrollView *parentScrollView;
+
+/**
+ An editable field to allow the user to modify the
+ text of a node.
+ **/
+@property(retain) IBOutlet 	UITextField *labelField;
+
+#else
 /**
  Scroll view reference.
  Used if the view is nested within a parent view.
@@ -123,6 +171,7 @@
  **/
 @property(retain) IBOutlet 	NSTextField *labelField;
 
+#endif
 /**
  Save the graph to the supplied file path.
  **/
